7933e33156930616db17e0fc920b984cbae40cbb
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-07-08 17:49:20 +0900

    mapbox add

---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField][Tag] private string hoge;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
