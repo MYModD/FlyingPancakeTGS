67eda112518ed774589ef793f629da8b1186b1ef
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-07-16 17:10:23 +0900

    ぶっちぎりバーサーカーからスクリプト持ってきた その改良

--- Assets/Folder_Miyamo/Script/TagSelector.cs
+++ Assets/Folder_Miyamo/Script/TagSelector.cs
@@ -4,29 +4,19 @@ using UnityEngine.InputSystem;
 
 public class TagSelector : MonoBehaviour
 {
-    private FlyingPancakeTGS _flyingPancakeTGS;
+    private GamePadController _inController;
 
-    [SerializeField, Tag]
-    private string _testTag;
+
+
     private void Start()
     {
-        _flyingPancakeTGS = new FlyingPancakeTGS();
-        _flyingPancakeTGS.Enable();
     }
+
     private void Update()
     {
-        if (_flyingPancakeTGS.Player.Fire.triggered)
-        {
-            print("hoge");
-            Gamepad.current.SetMotorSpeeds(12f, 12f);
-        }
-        if (_flyingPancakeTGS.Player.Move.triggered)
-        {
-            print("hoge");
-            Gamepad.current.SetMotorSpeeds(0, 0f);
-        }
-        Debug.Log(_flyingPancakeTGS.Player.Move.ReadValue<Vector2>());
-        Debug.Log(_flyingPancakeTGS.Player.Speed.ReadValue<Vector2>().y);
+        
+        //Debug.Log(Gamepad.current.leftStick.ReadValue());
+        
 
     }
 }
