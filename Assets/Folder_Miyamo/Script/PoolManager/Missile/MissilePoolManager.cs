804ccf86e631f62ae82fc5ecf7373c46549cd4e5
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-07-19 18:55:02 +0900

    push

--- Assets/Folder_Miyamo/Script/PoolManager/Missile/MissilePoolManager.cs
+++ Assets/Folder_Miyamo/Script/PoolManager/Missile/MissilePoolManager.cs
@@ -8,10 +8,10 @@ public class MissilePoolManager : PoolManager<TestMissile>
    
     public void FireMissile(Transform enemyTarget,Transform firePostion)
     {
+        
         TestMissile missile = _objectPool.Get();
-
         missile.Initialize();                       //èâä˙âª
-        missile.transform.position = firePostion.position;
+        missile.transform.SetPositionAndRotation(firePostion.position, firePostion.rotation);
         missile._enemyTarget = enemyTarget;
 
     }
