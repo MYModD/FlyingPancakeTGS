2c7aa495324b74d4b2fab0d29f183cb07fd9236a
RS7027 <167073147+RS7027@users.noreply.github.com>
2024-07-22 19:50:06 +0900

    ssss

--- Assets/Folder_Sakai/Script/ApparentScaleChange.cs
+++ Assets/Folder_Sakai/Script/ApparentScaleChange.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApparentScaleChange : MonoBehaviour
-{
+public class ApparentScaleChange : MonoBehaviour {
     // カメラからの距離が1のときのスケール値
     Vector3 _baseScale;
+
+    // スクリプトの動作を有効/無効にするフラグ
+    [SerializeField] private bool _isActive = true;
 
     void Start() {
         // カメラからの距離が1のときのスケール値を算出
@@ -13,11 +15,19 @@ public class ApparentScaleChange : MonoBehaviour
     }
 
     void Update() {
-        this.transform.localScale = this._baseScale * this.GetDistance();
+        // スクリプトが有効な場合のみスケールを更新
+        if (_isActive) {
+            this.transform.localScale = this._baseScale * this.GetDistance();
+        }
     }
 
     // カメラからの距離を取得
     float GetDistance() {
         return (this.transform.position - Camera.main.transform.position).magnitude;
+    }
+
+    // スクリプトの動作を切り替えるメソッド
+    public void ToggleActive() {
+        _isActive = !_isActive;
     }
 }
