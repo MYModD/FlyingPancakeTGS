6f6b7b1ef03e4f01f8a7af108ef9ac3afb8461f8
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-07-24 12:24:57 +0900

    クールタイムつけたよ

--- Assets/Folder_Miyamo/Script/GameController.cs
+++ Assets/Folder_Miyamo/Script/GameController.cs
@@ -1,49 +1,58 @@
 using NaughtyAttributes;
 using UnityEngine;
-using UnityEngine.Splines;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     [Foldout("ミサイル係")]
-    [SerializeField,Header("ミサイル発射位置")]
-    private Transform _fireMissilePostion;
+    [SerializeField, Header("ミサイル発射位置")]
+    private Transform _fireMissilePosition;
 
     [Foldout("ミサイル係")]
-    [SerializeField] 
-    private MissilePoolManager _missilePoolManger;
+    [SerializeField]
+    private MissilePoolManager _missilePoolManager;
+
+    [Foldout("ミサイル係")]
+    [SerializeField, Header("クールタイム")]
+    private float _missileCoolTime　= 1f;
 
     [Foldout("ロックオン系")]
     [SerializeField]
-    private  TestLockOnManager _testLockOnManager;
+    private TestLockOnManager _testLockOnManager;
 
     [Foldout("弾丸")]
     [SerializeField]
     private BulletPoolManager _bulletPoolManager;
+
     [Foldout("弾丸")]
     [SerializeField, Header("弾丸発射位置")]
-    private Transform _fireBulletPostion;
+    private Transform _fireBulletPosition;
+
     [Foldout("弾丸")]
     [SerializeField, Header("弾丸の速度")]
-    private float _bulletMultiply;
+    private float _bulletSpeedMultiplier;
+
+    private float _missileCooldownTimer; // タイマー計算用
+
+    void Update() {
 
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))     //ここをinputsystemに移行するとき直す
+        // ミサイル発射のクールタイム計算
+        _missileCooldownTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _missileCooldownTimer <= 0) //ここをInput Systemに移行するとき直す
         {
-            var enemy = _testLockOnManager._targetsInCone;
+            var enemies = _testLockOnManager._targetsInCone;
 
-            foreach (Transform item in enemy)
-            {
-                _missilePoolManger.FireMissile(item, _fireMissilePostion);
+            foreach (Transform enemy in enemies) {
+
+                _missilePoolManager.FireMissile(enemy, _fireMissilePosition);
             }
 
+            // クールタイムをリセット
+            _missileCooldownTimer = _missileCoolTime;
         }
 
-
-        if (Input.GetKey(KeyCode.K))
-        {
-            _bulletPoolManager.FireBullet(_fireBulletPostion,_bulletMultiply);
+        if (Input.GetKey(KeyCode.K)) {
+            _bulletPoolManager.FireBullet(_fireBulletPosition, _bulletSpeedMultiplier);
         }
     }
 }
