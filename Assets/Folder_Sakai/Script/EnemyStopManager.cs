47c06dc1c09650919675b6cf54c09cfcff0affd5
RS7027 <167073147+RS7027@users.noreply.github.com>
2024-07-25 18:32:32 +0900

    ss

--- Assets/Folder_Sakai/Script/EnemyStopManager.cs
+++ Assets/Folder_Sakai/Script/EnemyStopManager.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStopManager : MonoBehaviour
-{
+public class EnemyStopManager : MonoBehaviour {
+
+    [SerializeField] private GameObject[] _stopObjs;
     bool _isTheStateStopped;
+    private float _stopSpeed =0;
+    private float _moveSpeed =100;
 
     public void EnemyMovementOrStopped(bool isTheStateStopped) {
 
         _isTheStateStopped = isTheStateStopped;
+
+        foreach (GameObject obj in _stopObjs) {
+
+            BattleshipController battleship = obj.GetComponent<BattleshipController>();
+            if (battleship != null) {
+
+                if (_isTheStateStopped) {
+
+                    battleship.ChageSpeed(_stopSpeed);
+                } else {
+
+                    battleship.ChageSpeed(_moveSpeed);
+                }
+                
+            }
+        }
 
     }
 }
