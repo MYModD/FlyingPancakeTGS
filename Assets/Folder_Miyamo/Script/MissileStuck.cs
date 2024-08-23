2ce27a76a6ffd3909d7681dd0f45ec9b8f0b048a
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-08-23 18:44:27 +0900

    hoge

--- Assets/Folder_Miyamo/Script/MissileStuck.cs
+++ Assets/Folder_Miyamo/Script/MissileStuck.cs
@@ -6,53 +6,7 @@ using UnityEngine;
 
 public class MissileStuck : MonoBehaviour
 {
-    [ReadOnly,Header("書き換え不可")]
-    public Transform _enemyTarget = default;
-
-    [Header("ロックオンできる時間")]
-    public float _timer = 5f;
-
-    [Header("代入可能か")]
-    public bool _isValueAssignable = default;
-
     
-    private void Start() {
-        // 初期化
-        Initialize();
-    }
-
-
-    //ロックオンできるかと値を代入できるかの2つのbool値が必要なので改善する必要がある
-    //撃ったあとに再び撃てるようにするプログラムが必要なのだ
-    public void TargetLockOn(Transform targetEnemy) {
-        _isValueAssignable = false;
-        _enemyTarget = targetEnemy;
-
-        StartCoroutine(nameof(StartTimer));
-    }
-
-
-
-    /// <summary>
-    /// 一定時間後に初期化するコルーチン
-    /// </summary>
-    IEnumerator StartTimer() {
-        yield return new WaitForSeconds(_timer);
-        Initialize();
-    }
-
-
-
-    /// <summary>
-    /// 初期化、
-    /// </summary>
-    public void Initialize() {
-
-        _isValueAssignable = true;
-        _enemyTarget = null;
-
-    }
-
 
 
 }
