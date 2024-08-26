bc70f19a0c15959ffe2b4b8baba4513f4692129e
BeeDai <168399718+BeeDai@users.noreply.github.com>
2024-08-26 14:45:53 +0900

    もう大丈夫！なぜかって？そう！私が来た！
    1つのプレイヤーで5ステージいけるようになったわよ

--- Assets/Folder_Saito/Script/Resultet.cs
+++ Assets/Folder_Saito/Script/Resultet.cs
@@ -5,36 +5,8 @@ using UnityEngine;
 public class Resultet : MonoBehaviour {
     [SerializeField] private CanvasManager _canvas;
 
-    [Header("1stStage")]
-    [SerializeField, Header("1stPlayerのタグ"), Tag] private string _tag1st;
-    [SerializeField, Header("1stのプレイヤー")] private GameObject _player1st;
-    [SerializeField, Header("1stのステージ")] private GameObject _game1st;
-    private bool _isGame1st = true;
-
-    [Header("2stStage")]
-    [SerializeField, Header("2stPlayerのタグ"), Tag] private string _tag2st;
-    [SerializeField, Header("2stのプレイヤー")] private GameObject _player2st;
-    [SerializeField, Header("2stのステージ")] private GameObject _game2st;
-    private bool _isGame2st = true;
-
-    [Header("3stStage")]
-    [SerializeField, Header("3stPlayerのタグ"), Tag] private string _tag3st;
-    [SerializeField, Header("3stのプレイヤー")] private GameObject _player3st;
-    [SerializeField, Header("3stのステージ")] private GameObject _game3st;
-    private bool _isGame3st = true;
-
-    [Header("4stStage")]
-    [SerializeField, Header("4stPlayerのタグ"), Tag] private string _tag4st;
-    [SerializeField, Header("4stのプレイヤー")] private GameObject _player4st;
-    [SerializeField, Header("4stのステージ")] private GameObject _game4st;
-    private bool _isGame4st = true;
-
-    [Header("5stStage")]
-    [SerializeField, Header("5stPlayerのタグ"), Tag] private string _tag5st;
-    [SerializeField, Header("5stのプレイヤー")] private GameObject _player5st;
-    [SerializeField, Header("5stのステージ")] private GameObject _game5st;
-    private bool _isGame5st = true;
-
+    [SerializeField,Header("CutInのアニメーター")] private Animator _animator;
+    [SerializeField, Header("1stPlayerのタグ"), Tag] private string _playerTag;
     // Start is called before the first frame update
     void Start() {
 
@@ -45,26 +17,8 @@ public class Resultet : MonoBehaviour {
 
     }
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag(_tag1st) && _isGame1st) {
-
-            _isGame1st = false;
+        if (other.CompareTag(_playerTag)) {
+            _animator.Play("CutIN");
         }
-        if (other.CompareTag(_tag2st) && _isGame2st) {
-
-            _isGame2st = false;
-        }
-        if (other.CompareTag(_tag3st) && _isGame3st) {
-
-            _isGame3st = false;
-        }
-        if (other.CompareTag(_tag4st) && _isGame4st) {
-
-            _isGame4st = false;
-        }
-        if (other.CompareTag(_tag5st) && _isGame5st) {
-
-            _isGame5st = false;
-        }
-
     }
 }
