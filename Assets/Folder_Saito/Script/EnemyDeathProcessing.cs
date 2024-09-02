04566ddc20e59634dab164ae267b329fa96fc6b3
BeeDai <168399718+BeeDai@users.noreply.github.com>
2024-09-02 09:27:17 +0900

    UpdeteにOtherはねぇよ

--- Assets/Folder_Saito/Script/EnemyDeathProcessing.cs
+++ Assets/Folder_Saito/Script/EnemyDeathProcessing.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDeathProcessing : MonoBehaviour
-{
+public class EnemyDeathProcessing : MonoBehaviour {
     [SerializeField] private CountTheNumberOfDefeats _countTheNumberOfDefeats;
     [SerializeField, Tag] private string _missileTag;
-    [SerializeField] private Plane _plane;
+    [SerializeField] private Planeee _planeee;
     [SerializeField] private Rigidbody _rigidbody;
-    private bool _isDeath=true;
+    private bool _isDeath = true;
 
-    private void Update() {
+    private void OnTriggerEnter(Collider other) {
 
-        if (other.gameObject.CompareTag(_missileTag)&&_isDeath) {
+        if (other.gameObject.CompareTag(_missileTag) && _isDeath) {
 
+            _planeee.hp = -100;
             _countTheNumberOfDefeats.AdditionOfNumberOfDefeats();
             _rigidbody.useGravity = true;
             this.gameObject.transform.parent = null;
