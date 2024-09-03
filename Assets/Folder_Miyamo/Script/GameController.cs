17b1d7946af8258c4b4e225c8f565aa6109dfaf7
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-03 19:50:47 +0900

    鉄砲復活

--- Assets/Folder_Miyamo/Script/GameController.cs
+++ Assets/Folder_Miyamo/Script/GameController.cs
@@ -5,55 +5,72 @@ public class GameController : MonoBehaviour {
 
     #region 変数
 
-    
-    [Foldout("ミサイル係")]
-    [SerializeField, Header("ミサイル発射位置")]
-    private Transform _fireMissilePosition;
-
-    [Foldout("ミサイル係")]
-    [SerializeField]
-    private MissilePoolManager _missilePoolManager;
-
-    [Foldout("ミサイル係")]
-    [SerializeField, Header("クールタイム")]
-    private float _missileFireCoolTime　= 1f;
-
     [Foldout("ロックオン系")]
     [SerializeField]
     private TestLockOnManager _testLockOnManager;
 
-    
-    
-    
+    [Foldout("ミサイル発射")]
+    [SerializeField, Header("ミサイル発射位置")]
+    private Transform _missileLaunchPosition;
 
-    private float _missileCooldownTimer; // タイマー計算用
+    [Foldout("ミサイル発射")]
+    [SerializeField]
+    private MissilePoolManager _missilePoolManager;
 
+    [Foldout("ミサイル発射")]
+    [SerializeField, Header("ミサイル発射間隔")]
+    [Range(0,1f)]
+    private float _missileFireInterval = 0.5f;
+
+    [Foldout("弾丸発射")]
+    [SerializeField]
+    private BulletPoolManager _bulletPoolManager;
+
+    [Foldout("弾丸発射")]
+    [SerializeField, Header("弾丸発射位置")]
+    private Transform _bulletLaunchPosition;
+
+    [Foldout("弾丸発射")]
+    [SerializeField, Header("弾丸速度倍率")]
+    private float _bulletSpeedMultiplier;
+
+    [Foldout("弾丸発射")]
+    [SerializeField, Header("弾丸発射間隔")]
+    [Range(0, 1f)]
+    private float _bulletFireInterval  = 0.5f;
+
+    private float _missileCooldownTimer; // ミサイル発射までの残り時間
+    private float _bulletCooldownTimer;   // 弾丸発射までの残り時間
 
     #endregion
 
-
-
     #region メソッド
-    
+
     void Update() {
-
-
-        // ミサイル発射のクールタイム計算
+        // ミサイル発射クールタイムの更新
         _missileCooldownTimer -= Time.deltaTime;
 
-        // スペースもしくはGamePadの下ボタンを押していてかつクールタイムが0以下のとき
-        bool canFire = (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Submit"))
-             && (_missileCooldownTimer <= 0);
+        // ミサイル発射条件判定
+        bool canFireMissile = (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Submit"))
+                              && _missileCooldownTimer <= 0;
 
-
-        if (canFire)       
-        {
-            _missilePoolManager.FireMissiles(_fireMissilePosition);
-            _missileCooldownTimer = _missileFireCoolTime;
-            Debug.Log("ボタン押した");
+        if (canFireMissile) {
+            _missilePoolManager.FireMissiles(_missileLaunchPosition);
+            _missileCooldownTimer = _missileFireInterval;
+            Debug.Log("ミサイル発射");
         }
 
-        
+        // 弾丸発射クールタイムの更新
+        _bulletCooldownTimer -= Time.deltaTime;
+
+        // 弾丸発射条件判定
+        bool canFireBullet = (Input.GetKeyDown(KeyCode.K) || Input.GetButton("Fire1"))
+                              && _bulletCooldownTimer <= 0;
+
+        if (canFireBullet) {
+            _bulletPoolManager.FireBullet(_bulletLaunchPosition, _bulletSpeedMultiplier);
+            _bulletCooldownTimer = _bulletFireInterval;
+        }
     }
     #endregion
 }
--- Assets/Folder_Miyamo/Script/PoolManager/Bullet/BulletPoolManager.cs
+++ Assets/Folder_Miyamo/Script/PoolManager/Bullet/BulletPoolManager.cs
@@ -7,10 +7,38 @@ public class BulletPoolManager : PoolManager<Bullet> {
     /// <param name="playerPosition">playerの位置</param>
     /// <param name="firePosition">弾丸発射の位置</param>
     /// <param name="multiplyValue">弾の速度,掛ける</param>
-    public void FireBullet(Transform playerPosition, Transform firePosition, float multiplyValue) {
-        
+
+
+
+
+    public void FireBullet(Transform firePosition, float multiplyValue) {
+
         // オブジェクトプールから弾を取得、初期化
-        var bullet = _objectPool.Get();
+        Bullet bullet = _objectPool.Get();
+        bullet.Initialize();
+
+
+        // 弾の位置と回転を発射位置と同じに設定 こっちのほうが軽いらしい
+        bullet.transform.SetPositionAndRotation(firePosition.position, firePosition.rotation);
+
+        // 弾に力を加えて発射
+        bullet.GetComponent<Rigidbody>().velocity = transform.forward *  multiplyValue;
+
+    }
+
+
+
+
+
+
+
+
+
+
+    /*public void FireBullet(Transform playerPosition, Transform firePosition, float multiplyValue) {
+
+        // オブジェクトプールから弾を取得、初期化
+        Bullet bullet = _objectPool.Get();
         bullet.Initialize();             
 
 
@@ -24,5 +52,5 @@ public class BulletPoolManager : PoolManager<Bullet> {
 
         // 弾に力を加えて発射
         bullet.GetComponent<Rigidbody>().velocity = firingDirection * multiplyValue;
-    }
+    }*/
 }
