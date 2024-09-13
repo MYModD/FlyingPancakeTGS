a3119d195be9870be846f383e8945305334ef826
BeeDai <168399718+BeeDai@users.noreply.github.com>
2024-09-13 13:57:00 +0900

    Update EnemyDeathProcessing.cs

--- Assets/Folder_Saito/Script/EnemyDeathProcessing.cs
+++ Assets/Folder_Saito/Script/EnemyDeathProcessing.cs
@@ -7,14 +7,18 @@ public class EnemyDeathProcessing : MonoBehaviour {
     [SerializeField, Tag] private string _missileTag;
     [SerializeField] private Planeee _planeee;
     [SerializeField] private Rigidbody _rigidbody;
+    private ControllerBuruBuru _controller;
     private bool _isDeath = true;
 
 
     private void OnTriggerEnter(Collider other) {
-
+        if (_controller == null) {
+            _controller = ControllerBuruBuru.Instance;
+        }
         if (other.gameObject.CompareTag(_missileTag) && _isDeath) {
             _planeee.hp = -100;
             _countTheNumberOfDefeats.AdditionOfNumberOfDefeats();
+            _controller.StartVibration();
             _rigidbody.useGravity = true;
             this.gameObject.transform.parent = null;
         }
