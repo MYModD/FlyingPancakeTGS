89408b66617f9cd8c34237c6ea167f606b28c141
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-13 17:21:49 +0900

    ゲージの割合GamePad二追加

--- Assets/Folder_Miyamo/Script/GameController.cs
+++ Assets/Folder_Miyamo/Script/GameController.cs
@@ -2,9 +2,7 @@ using NaughtyAttributes;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-
     #region 変数
-
     [Foldout("ロックオン系")]
     [SerializeField]
     private TestLockOnManager _testLockOnManager;
@@ -19,21 +17,21 @@ public class GameController : MonoBehaviour {
 
     [Foldout("ミサイル発射")]
     [SerializeField, Header("ミサイル発射間隔")]
-    [Range(0,3f)]
+    [Range(0, 3f)]
     private float _missileFireInterval = 0.5f;
 
-    
+    [Header("クールタイムの割合"), ReadOnly]
+    public float _coolTimeRatio;
 
-    private  float _missileCooldownTimer; // ミサイル発射までの残り時間
+    private float _missileCooldownTimer; // ミサイル発射までの残り時間
     private float _bulletCooldownTimer;   // 弾丸発射までの残り時間
-
     #endregion
 
     #region メソッド
-
     void Update() {
         // ミサイル発射クールタイムの更新
         _missileCooldownTimer -= Time.deltaTime;
+        _missileCooldownTimer = Mathf.Max(_missileCooldownTimer, 0f); // 0未満にならないようにする
 
         // ミサイル発射条件判定
         bool canFireMissile = (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Submit"))
@@ -45,6 +43,8 @@ public class GameController : MonoBehaviour {
             Debug.Log("ミサイル発射");
         }
 
+        // クールタイムの割合を計算（0~1の範囲に収める）
+        _coolTimeRatio = 1f - Mathf.Clamp01(_missileCooldownTimer / _missileFireInterval);
     }
     #endregion
 }
