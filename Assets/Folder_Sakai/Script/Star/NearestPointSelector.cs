d84a185b56bb2214dfdfaf2604af5ab8cb87a948
RS7027 <167073147+RS7027@users.noreply.github.com>
2024-09-17 09:56:44 +0900

    s

--- Assets/Folder_Sakai/Script/Star/NearestPointSelector.cs
+++ Assets/Folder_Sakai/Script/Star/NearestPointSelector.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class NearestPointSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private Transform[] _points; // 指定する地点をシリアライズフィールドで指定
+
+    private void OnTriggerEnter(Collider other) {
+        if (_points.Length == 0) {
+            Debug.LogWarning("ポイントが指定されていません");
+            return;
+        }
+
+        Transform nearestPoint = GetNearestPoint(other.transform.position);
+        if (nearestPoint != null) {
+            Debug.Log("最も近いポイント: " + nearestPoint.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Transform GetNearestPoint(Vector3 position) {
+        Transform nearest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (Transform point in _points) {
+            float distance = Vector3.Distance(position, point.position);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearest = point;
+            }
+        }
+
+        return nearest;
     }
 }
