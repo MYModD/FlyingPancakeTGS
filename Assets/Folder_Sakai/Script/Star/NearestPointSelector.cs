19f756b6758e5419f6b0b4f127dd21441270b817
RS7027 <167073147+RS7027@users.noreply.github.com>
2024-09-17 11:33:33 +0900

    s

--- Assets/Folder_Sakai/Script/Star/NearestPointSelector.cs
+++ Assets/Folder_Sakai/Script/Star/NearestPointSelector.cs
@@ -2,28 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NearestPointSelector : MonoBehaviour
-{
-    [SerializeField] private Transform[] _points; // 指定する地点をシリアライズフィールドで指定
+public class NearestPointSelector : MonoBehaviour {
+
+    [SerializeField] private GameObject[] _points; // 指定する地点をシリアライズフィールドで指定
+    [SerializeField, Tag] private string _f14Tag;
 
     private void OnTriggerEnter(Collider other) {
-        if (_points.Length == 0) {
-            Debug.LogWarning("ポイントが指定されていません");
-            return;
+
+        if (other.gameObject.CompareTag(_f14Tag)) {
+
+
+            if (_points.Length == 0) {
+                Debug.LogWarning("ポイントが指定されていません");
+                return;
+            }
+
+            GameObject nearestPoint = GetNearestPoint(other.transform.position);
+            if (nearestPoint != null) {
+                Debug.Log("最も近いポイント: " + nearestPoint.name);
+
+                // ここでスクリプトを取得し、メソッドを実行する
+                var targetScript = nearestPoint.GetComponent<StarScore>();
+                if (targetScript != null) {
+                    targetScript.ScoreJudgment(); // メソッドを実行
+                } else {
+                    Debug.LogWarning("TargetScriptが見つかりません");
+                }
+            }
         }
 
-        Transform nearestPoint = GetNearestPoint(other.transform.position);
-        if (nearestPoint != null) {
-            Debug.Log("最も近いポイント: " + nearestPoint.name);
-        }
     }
 
-    private Transform GetNearestPoint(Vector3 position) {
-        Transform nearest = null;
+    private GameObject GetNearestPoint(Vector3 position) {
+        GameObject nearest = null;
         float minDistance = Mathf.Infinity;
 
-        foreach (Transform point in _points) {
-            float distance = Vector3.Distance(position, point.position);
+        foreach (GameObject point in _points) {
+            float distance = Vector3.Distance(position, point.transform.position);
             if (distance < minDistance) {
                 minDistance = distance;
                 nearest = point;
