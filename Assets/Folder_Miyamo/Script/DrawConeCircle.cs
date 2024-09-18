d98611577c0e594f809806b4e91c948436fa98a6
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-18 11:01:37 +0900

    hunga

--- Assets/Folder_Miyamo/Script/DrawConeCircle.cs
+++ Assets/Folder_Miyamo/Script/DrawConeCircle.cs
@@ -1,58 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DrawConeCircle : MonoBehaviour {
+
     public Camera _mainCamera;
+
     [SerializeField, Header("CameraManger")]
     private TestLockOnManager _testLockOnManager;
-    public bool _debugIsON = false;
-    private RectTransform _rectTransform;
 
-    [SerializeField]
-    private Text _circleCenterPositionText;
+    public bool _debugIsON = false;
 
     void Start() {
-        _rectTransform = GetComponent<RectTransform>();
-        if (_rectTransform == null) {
-            Debug.LogError("RectTransformが見つかりません。");
-        }
 
-        if (_circleCenterPositionText == null) {
-            Debug.LogError("Circle Center Position Textが割り当てられていません。");
-        }
     }
 
+    // Update is called once per frame
     private void LateUpdate() {
-        UpdateCirclePosition();
-    }
 
-    private void UpdateCirclePosition() {
-        // ワールド座標からスクリーン座標への変換
-        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(_mainCamera, _testLockOnManager._circleCenterPostion);
 
-        // スクリーン座標からUI座標への変換
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            _rectTransform.parent as RectTransform,
-            screenPoint,
-            null,
-            out Vector2 localPoint
-        );
-
-        // UI要素の位置を設定
-        _rectTransform.anchoredPosition = localPoint;
-
-        // UI要素のサイズを設定
+        Vector3 hoge = RectTransformUtility.WorldToScreenPoint(_mainCamera, _testLockOnManager._circleCenterPostion);
+        this.transform.position = hoge;
         float scale = _testLockOnManager._coneAngle * 0.1339f;
-        _rectTransform.sizeDelta = new Vector2(100 * scale, 100 * scale); // 基本サイズを100とした場合
-    }
+        transform.localScale = new Vector3(scale, scale, scale);
 
-    
+    }
 
     private void OnDrawGizmos() {
+
         if (!_debugIsON) {
-            UpdateCirclePosition();
+
+            Vector3 hoge = RectTransformUtility.WorldToScreenPoint(_mainCamera, _testLockOnManager._circleCenterPostion);
+            this.transform.position = hoge;
+            float scale = _testLockOnManager._coneAngle * 0.1339f;
+            transform.localScale = new Vector3(scale, scale, scale);
+
+
+
+            //Quaternion hoge = _testLockOnManager._circleRotation;
+
+            //transform.rotation = hoge;
+            //transform.position = _testLockOnManager._circleCenterPostion;
+
+            //float scale = _testLockOnManager._circleRadius * 0.01487f;
+            //transform.localScale = new Vector3(scale, scale, scale);
+
         }
+
     }
 }
