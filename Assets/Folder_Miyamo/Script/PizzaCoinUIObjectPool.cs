b8bc35c92069a8678bb9f17c30faa837f23da841
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-20 05:47:28 +0900

    pizza is  256 million color ,right?

--- Assets/Folder_Miyamo/Script/PizzaCoinUIObjectPool.cs
+++ Assets/Folder_Miyamo/Script/PizzaCoinUIObjectPool.cs
@@ -1,24 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PizzaCoinUIObjectPool : PoolManager<PizzaCoinUI> {
-
-    [SerializeField,Range(0,50f)]
-    private float _widthRandom;
+    [SerializeField, Range(0, 50f)]
+    private float _minWidthRandom = 0f;
 
     [SerializeField, Range(0, 50f)]
-    private float _scaleYrandom;
+    private float _maxWidthRandom = 50f;
 
+    [SerializeField, Range(0, 50f)]
+    private float _minScaleYRandom = 0f;
+
+    [SerializeField, Range(0, 50f)]
+    private float _maxScaleYRandom = 50f;
+
+    private int _instanceCount = 0;
 
     protected override PizzaCoinUI Create() {
         PizzaCoinUI instance = Instantiate(_pooledPrefab, transform.position, Quaternion.identity, transform);
         instance.ObjectPool = _objectPool;
 
-        Vector2 uiScale = new Vector2(100 - Random.Range(_widthRandom, _widthRandom), 100 - Random.Range(_scaleYrandom, _scaleYrandom));
-        instance.GetComponent<RectTransform>().sizeDelta = uiScale;
+        _instanceCount++;
+
+        float randomWidth = Random.Range(_minWidthRandom, _maxWidthRandom);
+        float randomHeight = Random.Range(_minScaleYRandom, _maxScaleYRandom);
+        Vector2 uiScale = new Vector2(100 - randomWidth, 100 - randomHeight);
+
+        Debug.Log($"Instance {_instanceCount}: UI Scale = {uiScale}");
+
+        RectTransform rectTransform = instance.GetComponent<RectTransform>();
+        if (rectTransform != null) {
+            rectTransform.sizeDelta = uiScale;
+        } else {
+            Debug.LogWarning("RectTransform component not found on the instantiated PizzaCoinUI.");
+        }
+
         return instance;
     }
-
-
 }
