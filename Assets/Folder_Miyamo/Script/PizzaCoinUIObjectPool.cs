183c40d4b36f072433a11a7259e34ffb843df2dc
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-19 21:22:12 +0900

    cash

--- Assets/Folder_Miyamo/Script/PizzaCoinUIObjectPool.cs
+++ Assets/Folder_Miyamo/Script/PizzaCoinUIObjectPool.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PizzaCoinUIObjectPool : PoolManager<PizzaCoinUI> {
 
+    [SerializeField,Range(0,50f)]
+    private float _widthRandom;
+
+    [SerializeField, Range(0, 50f)]
+    private float _scaleYrandom;
+
 
     protected override PizzaCoinUI Create() {
         PizzaCoinUI instance = Instantiate(_pooledPrefab, transform.position, Quaternion.identity, transform);
         instance.ObjectPool = _objectPool;
+
+        Vector2 uiScale = new Vector2(100 - Random.Range(_widthRandom, _widthRandom), 100 - Random.Range(_scaleYrandom, _scaleYrandom));
+        instance.GetComponent<RectTransform>().sizeDelta = uiScale;
         return instance;
     }
 
