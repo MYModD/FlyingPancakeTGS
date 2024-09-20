9bfd4beab947dba968e47a8f0daba5a6363eb6c9
BeeDai <168399718+BeeDai@users.noreply.github.com>
2024-09-20 13:48:29 +0900

    うに

--- Assets/Folder_Saito/Script/GetCooltime.cs
+++ Assets/Folder_Saito/Script/GetCooltime.cs
@@ -5,36 +5,42 @@
 // 作成者:
 // ---------------------------------------------------------
 using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
-public class GetCooltime : MonoBehaviour
-{
+
+public class GetCooltime : MonoBehaviour {
     #region 変数
     [SerializeField] private Slider _coolTimeSlider;
-    [SerializeField]
-    private GameController _gameController;
+
+    [Header("Game Controller Reference")]
+    [SerializeField] private GameController _gameController;
+
+    [Header("Image to Fade from White to Black")]
+    [SerializeField] private Image _coolTimeImage;
+
+    [SerializeField] private float _fadeDuration = 5f; // 色を変化させる時間（秒）
+    private float _elapsedTime = 0f;
     #endregion
+
     #region プロパティ
     #endregion
+
     #region メソッド
-/// <summary>
-/// 初期化処理 使わないなら消す
-/// </summary>
-void Awake()
-{
-}
-/// <summary>
-/// 更新前処理
-/// </summary>
-void Start ()
-{
-}
-/// <summary>
-/// 更新処理
-/// </summary>
-void Update ()
-{
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update() {
+        // クールタイムスライダーの更新
         _coolTimeSlider.value = _coolTimeSlider.maxValue - _gameController.CoolTime();
-}
-#endregion
+
+        if (_coolTimeSlider.value != 2) {
+            _coolTimeImage.color = Color.grey;
+        }
+        else {
+            _coolTimeImage.color = Color.yellow;
+        }
+
+    }
+
+    #endregion
 }
