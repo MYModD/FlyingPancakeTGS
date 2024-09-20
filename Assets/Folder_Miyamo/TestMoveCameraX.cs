6e7765209651feaf6190832018c6279d63d01a3b
Mona Lisa <20237086-miyamotoayumu@hcs.ac.jp>
2024-09-20 15:20:32 +0900

    ナオッダ！！

--- Assets/Folder_Miyamo/TestMoveCameraX.cs
+++ Assets/Folder_Miyamo/TestMoveCameraX.cs
@@ -18,5 +18,10 @@ public class TestMoveCameraX : MonoBehaviour
             transform.position += new Vector3(10, 0, 0);
         
         }
+        if (Input.GetKey(KeyCode.S)) {
+
+            transform.position -= new Vector3(10, 0, 0);
+
+        }
     }
 }
