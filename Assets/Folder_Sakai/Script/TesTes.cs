ad1b577dca078860f786bd2378e8b8e2a56da466
RS7027 <167073147+RS7027@users.noreply.github.com>
2024-09-24 14:48:31 +0900

    s

--- Assets/Folder_Sakai/Script/TesTes.cs
+++ Assets/Folder_Sakai/Script/TesTes.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TesTes : MonoBehaviour {
-    [SerializeField] private GameObject _as; // 対象のオブジェクト
+    [SerializeField] private GameObject _as; 
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Y)) {
-            // _as がアクティブなら非アクティブに、非アクティブならアクティブに切り替える
+
             bool isActive = _as.activeSelf;
             _as.SetActive(!isActive);
         }
